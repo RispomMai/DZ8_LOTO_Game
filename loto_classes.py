e16816c7845af8cd2d3ea4833a5aff026253210f
Александр Мельников <rispom@gmail.com>
2023-12-15 20:59:14 +0700

    initial

--- loto_classes.py
+++ loto_classes.py
@@ -6,7 +6,6 @@ class Player:
         self.human = True
         self.numbers_barrels = 0
         self.card = Card()
-
     def check(self, number):
         if self.card.numbers.get(number) != None:
             self.card.numbers[number] = 1
@@ -17,10 +16,7 @@ class Player:
 
 
 def pattern_card():
-    ###   position_empty_cell = random.sample(range(0, 26), 12)
-    ###   position_empty_cell.sort()
     pattern = [1] * 27
-
     for i in range(0, 3):
         pec = random.sample(range(0, 8), 4)
         pec.sort()
@@ -28,11 +24,19 @@ def pattern_card():
             pattern[ii + 9 * i] = 0
     return pattern
 
+def define_lines_card(s):
+    list_values =[]
+    for i in range(0,3):
+        ss = s[i*5:i*5+5]
+        ss.sort()
+        list_values += ss
+    return list_values
 
 class Card:
     def __init__(self):
         self.numbers = {i: 0 for i in random.sample(range(1, 90), 15)}
         self.pattern = pattern_card()
+        self.lines = define_lines_card(list(self.numbers.keys()))
 
 
 class Keg:
